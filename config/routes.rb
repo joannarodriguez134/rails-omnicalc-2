989a36e5fd424c671ff5c70ed50d64cf6b151d7d
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-02-07 16:42:16 +0000

    Fixed multiplication form and results

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
 
   get("/wizard_subtract", { :controller => "subtraction", :action => "subtract_these" })
 
-  get("/multiply", { :controller => "multiplication", :action => "multiplication" })
+  get("/multiply", { :controller => "multiplication", :action => "show_multiplication_form" })
 
-  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
+  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply_these" })
 
   get("/divide", { :controller => "division", :action => "show_division_form" })
   get("/wizard_div", { :controller => "div", :action => "divide_these" })
